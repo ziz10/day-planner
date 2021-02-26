53026ec4ace4f2a867754242a21c9a5fb42acf5e
Abdoul Bass <malickya8891@gmail.com>
2021-02-25 20:48:29 -0800

    modified script.js along with html file

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,37 @@
-// console.log('hello');
+
 // var task;
 // console.log('global', task)
 $(".txtb").on("keyup", function (e) {
     console.log(e.keyCode)
-    alert("ok");
+    // alert("ok");
     var task;
     console.log('inside key up', task)
     // var del;
-    if (e.keyCode === 13 && $(".txtb").val() !== "") {
+    if (e.keyCode === 13 && $(".txtb").val() !== "")
+     {
         // console.log(e.keyCode)
         var task = $("<div class='task'></div>").text($(".txtb").val());
         // console.log('inside if', task)
-        var del = $("<i class-'fa-trash-alt'></i>").click(function () {
+        var del = $("<i class='fa-trash-alt'></i>").click(function () {
             var p = $(this).parent();
             p.fadeOut(function () {
                 p.remove();
             });
         })
+    
+
+    var check = $("<i class='fas fa-check'></i>").click(function () {
+        console.log("this")
+        var p = $(this).parent();
+        p.fadeOut(function () {
+            $(".comp").append(p);
+            p.fadeIn();
+
+        })
+        $(this).remove();
+    });
+    task.append(del, check);
+    $(".notcomp").append(task);
+    $(".txtb").val("");
     }
-
-
 });
