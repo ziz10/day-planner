68ec145a252aeda70ddd4e0b311b44293fd5eb58
Abdoul Bass <malickya8891@gmail.com>
2021-02-25 19:08:05 -0800

    implemented logic for the check mark and delete button

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,20 @@
 // var task;
 // console.log('global', task)
 $(".txtb").on("keyup", function (e) {
-    // console.log(e.keyCode)
-    // alert("ok");
-    // var task;
-    // console.log('inside key up', task)
-    // // var del;
-    // if (e.keyCode === 13 && $(".txtb").val() !== "") {
-    //     // console.log(e.keyCode)
-    //     var task = $("<div class='task'></div>").text($(".txtb").val());
-    //     // console.log('inside if', task)
-    //     var del = $("<i class-'fa-trash-alt'></i>").click(function () {
-    //         var p = $(this).parent();
-    //         p.fadeOut(function () {
-    //             p.remove();
-    //         });
+    console.log(e.keyCode)
+    alert("ok");
+    var task;
+    console.log('inside key up', task)
+    // var del;
+    if (e.keyCode === 13 && $(".txtb").val() !== "") {
+        // console.log(e.keyCode)
+        var task = $("<div class='task'></div>").text($(".txtb").val());
+        // console.log('inside if', task)
+        var del = $("<i class-'fa-trash-alt'></i>").click(function () {
+            var p = $(this).parent();
+            p.fadeOut(function () {
+                p.remove();
+            });
         })
     }
 
