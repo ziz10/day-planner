d572538b3f35f72d1fdffea311293d52da6e4743
Abdoul Bass <malickya8891@gmail.com>
2021-02-25 19:04:17 -0800

    created alert()

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,17 @@ $(".txtb").on("keyup", function (e) {
     // console.log(e.keyCode)
     // alert("ok");
     // var task;
-    console.log('inside key up', task)
-    // var del;
-    if (e.keyCode === 13 && $(".txtb").val() !== "") {
-        // console.log(e.keyCode)
-        var task = $("<div class='task'></div>").text($(".txtb").val());
-        // console.log('inside if', task)
-        var del = $("<i class-'fa-trash-alt'></i>").click(function () {
-            var p = $(this).parent();
-            p.fadeOut(function () {
-                p.remove();
-            });
+    // console.log('inside key up', task)
+    // // var del;
+    // if (e.keyCode === 13 && $(".txtb").val() !== "") {
+    //     // console.log(e.keyCode)
+    //     var task = $("<div class='task'></div>").text($(".txtb").val());
+    //     // console.log('inside if', task)
+    //     var del = $("<i class-'fa-trash-alt'></i>").click(function () {
+    //         var p = $(this).parent();
+    //         p.fadeOut(function () {
+    //             p.remove();
+    //         });
         })
     }
 
